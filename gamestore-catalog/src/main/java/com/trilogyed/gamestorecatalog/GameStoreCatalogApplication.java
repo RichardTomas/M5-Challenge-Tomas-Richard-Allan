64b73d2ebb23c37c3991cd4d5169891566b4878a
RichardTomas <radtomas@gmail.com>
2022-10-30 21:12:59 -0700

    Game Store Catalog

---
+++ gamestore-catalog/src/main/java/com/trilogyed/gamestorecatalog/GameStoreCatalogApplication.java
@@ -0,0 +1,14 @@
+package com.trilogyed.gamestorecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class GameStoreCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreCatalogApplication.class, args);
+	}
+
+}
---
+++ gamestore-invoicing/src/main/java/com/trilogyed/gamestoreinvoicing/GameStoreInvoicingApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.gamestoreinvoicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreInvoicingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameStoreInvoicingApplication.class, args);
+	}
+
+}
